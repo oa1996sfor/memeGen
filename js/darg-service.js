de25334d1ac17087ae3a1fc21be428ada704f9df
omar amer <omar.amer.asfor.96@gmail.com>
2021-10-27 18:22:32 +0300

    drag fix

--- js/darg-service.js
+++ js/darg-service.js
@@ -5,7 +5,7 @@ var gIsDrag = false;
 function isTextClicked(clickedPos) {
     const pos = getLinePos();
     const distance = Math.sqrt((pos.x - clickedPos.x) ** 2 + (pos.y - clickedPos.y) ** 2);
-    return distance <= (getGmemeLineSize() * getGmemeText().length);
+    return distance <= ((getGmemeLineSize()/2) * getGmemeText().length);
 
 }
 
